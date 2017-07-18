5ef765d34742eef7bda38ce0c3eee33843807a05
jslusser <jslusser@gmail.com>
2017-07-18 15:09:55 -0400

    Initial commit

---
+++ Project23/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by James Slusser on 7/18/17.
+//  Copyright © 2017 James Slusser. All rights reserved.
+//  https://www.hackingwithswift.com/read/23/overview
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+    }
+    // Get label node from scene and store it for use later
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
